c89460ae4728b2bf5a829980ea795f6fe66ccdba
Tennessee Hickling <tennessee.wallaceh@gmail.com>
2025-07-03 19:47:18 +0100

    Add Poisson state-space model and tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -11,6 +11,7 @@ from seqjax import simulate, evaluate
 from seqjax.model.ar import AR1Target, ARParameters
 from seqjax.model.linear_gaussian import LinearGaussianSSM, LGSSMParameters
 from seqjax.model.stochastic_vol import SimpleStochasticVol, LogVolRW, TimeIncrement
+from seqjax.model.poisson_ssm import PoissonSSM, PoissonSSMParameters
 from seqjax.model.base import Prior, Transition, Emission, SequentialModel
 from seqjax.util import pytree_shape
 from tests.test_typing import (
@@ -65,6 +66,19 @@ def test_simple_stochastic_vol_simulate_length() -> None:
     assert obs.underlying.shape == (3,)
     assert pytree_shape(x_hist)[0][0] == 1
     assert pytree_shape(y_hist)[0][0] == 1
+
+
+def test_poisson_ssm_simulate_length() -> None:
+    key = jax.random.PRNGKey(0)
+    params = PoissonSSMParameters()
+    latent, obs, x_hist, y_hist = simulate.simulate(
+        key, PoissonSSM, None, params, sequence_length=3
+    )
+
+    assert latent.log_rate.shape == (3,)
+    assert obs.count.shape == (3,)
+    logp = evaluate.log_prob_joint(PoissonSSM, latent, obs, None, params)
+    assert jnp.shape(logp) == ()
 
 
 class Prior1(Prior[DummyParticle, DummyCondition, DummyParameters]):
