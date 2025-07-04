a8674ea03d4362915b1c82887ae89b9e7696debe
Tennessee Hickling <tennessee.wallaceh@gmail.com>
2025-07-04 09:31:37 +0100

    Refactor random walk MCMC to use blackjax

--- seqjax/inference/pmcmc/__init__.py
+++ seqjax/inference/pmcmc/__init__.py
@@ -1,3 +1,4 @@
-from .pmmh import RandomWalkConfig, ParticleMCMCConfig, run_particle_mcmc
+from seqjax.inference.mcmc.metropolis import RandomWalkConfig
+from .pmmh import ParticleMCMCConfig, run_particle_mcmc
 
 __all__ = ["RandomWalkConfig", "ParticleMCMCConfig", "run_particle_mcmc"]
