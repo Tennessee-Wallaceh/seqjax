69d67674bbbc8b837a0bda4bd8f8529e0953cf83
anon-8909 <anon@example.com>
2025-09-20 10:58:05 +0100

    configure softplus transformation

--- seqjax/inference/vi/run.py
+++ seqjax/inference/vi/run.py
@@ -42,6 +42,7 @@ def get_interval_spline():
 configured_bijections: dict[str, typing.Callable[[], transformations.Bijector]] = {
     "interval_spline": get_interval_spline,
     "sigmoid": partial(transformations.Sigmoid, lower=-1.0, upper=1.0),
+    "softplus": transformations.Softplus,
 }
 
 
